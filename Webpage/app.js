32aad5d90b0f1bc308032ff2a8c1112ffe965b7d
ChrispyPeaches <gameboy9478@gmail.com>
2022-09-30 13:28:16 -0500

    Implemented form & HTTP request functionality

--- Webpage/app.js
+++ Webpage/app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
                     htmlString +=
                         `
                     <div class="col pixel-box pixel">
-                        <input class="pixel-input" id="color-input-${i}-${j}" type="color">
+                        <input class="pixel-input" id="color-input-${i}-${j}" name="color-input-${i}-${j}" type="color" value="#923a3a">
                     </div>
                     `;
                 };
@@ -28,3 +28,33 @@ $(document).ready(function () {
     );
 });
 
+// Submits the HTTP Request of the pixels' colors to the given url in a JSON format
+function onSubmitPixelGridForm() {
+    // Converts the form data to be sent to API. Accepts an element from the page (expects a form element and gathers all of the inputs, then converts them to JSON)
+    function getFormData($form) {
+        var unindexed_array = $form.serializeArray();
+        var indexed_array = {};
+
+        $.map(unindexed_array, function (n, i) {
+            indexed_array[n['name']] = n['value'];
+        });
+
+        return indexed_array;
+    }
+    // Sends the request and gets a response without refreshing the web page.
+    $.ajax({
+        type: "POST",
+        url: 'index.html',
+        data: getFormData($("#pixel-form")),
+        dataType: "json",
+        contentType: 'application/json;charset=UTF-8',
+        // On a successful request, do the following.
+        success: function (response) {
+            console.log(response)
+        },
+        // On a failed request, do the following.
+        error: function (xhr, resp, text) {
+            console.log(text)
+        }
+    });
+}
