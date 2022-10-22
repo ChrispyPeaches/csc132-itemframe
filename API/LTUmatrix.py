e823b4be1645e0f9f031aa708cbbdc81ed09604c
cstrick13 <corystrickland13@gmail.com>
2022-10-21 20:43:03 -0500

    reads json and can pick color orders
    It works with multipple values of a json script but for one I did it in the wrong foramt and two struggling to actually get it from the web app

--- API/LTUmatrix.py
+++ API/LTUmatrix.py
@@ -1,16 +1,19 @@
 from time import sleep
 import board
 import neopixel
+import json
+import sys
 
-# PLUG DIN WIRE INTO PIN M18 ON PI BREAKOUT BOARD
-MATRIX_PIN = board.D12
+data = json.loads('{"#FFFFFF":3, "#B12345":78}')
+
+#PLUG DIN WIRE INTO PIN M18 ON PI BREAKOUT BOARD
+MATRIX_PIN = board.D12 
 
 # li = [ board.D11, board.D12, board.D13, board.D14, board.D15, board.D16, board.D17, board.D18, board.D19]
 # for i in li:
 #     print(i)
-pixels = neopixel.NeoPixel(board.D12, 256)
-pixels.deinit()
-sleep(1)
+
+
 pixels = neopixel.NeoPixel(board.D12, 256)
 sleep(1)
 #converts Hex values into rgb values function
@@ -19,17 +22,11 @@ def hex_to_rgb(value):
     return list(int(value[i:i+2],16)for i in (0,2,4))
 
 #uses the functionfrom above to return the pixel and hex value of the json
-def lightupMatrix(pixelArr, hex_values):
-    i = 0
-    for pixel in pixelArr:
-        pixels[pixel] = hex_to_rgb(hex_values[i])
-        i += 1
+def lightupMatrix(data):
+    for key, value in data.items():
+        pixels[value] = hex_to_rgb(key)
 
  
 
  
-pixel = [1,2]
-hex_value = ['#ff0066','FFFFFF']
-lightupMatrix(pixel,hex_value)
-sleep(2)
-pixels.deinit()
+lightupMatrix(data)
