bd8ed0bfad0fe9154d2f2533d19b6675855be757
cstrick13 <corystrickland13@gmail.com>
2022-10-23 17:18:01 -0500

    Working Led Matrix converter
    So went in and changed the way it takes data now it parses through the nest json and find the pixels and returns the pixel value as an number and hex as a string

--- API/LTUmatrix.py
+++ API/LTUmatrix.py
@@ -4,17 +4,15 @@ import neopixel
 import json
 import sys
 
-data = json.loads('{"#FFFFFF":3, "#B12345":78}')
 
 #PLUG DIN WIRE INTO PIN M18 ON PI BREAKOUT BOARD
-MATRIX_PIN = board.D12 
 
 # li = [ board.D11, board.D12, board.D13, board.D14, board.D15, board.D16, board.D17, board.D18, board.D19]
 # for i in li:
 #     print(i)
 
 
-pixels = neopixel.NeoPixel(board.D12, 256)
+pixels = neopixel.NeoPixel(board.D18, 256)
 sleep(1)
 #converts Hex values into rgb values function
 def hex_to_rgb(value):
@@ -23,10 +21,12 @@ def hex_to_rgb(value):
 
 #uses the functionfrom above to return the pixel and hex value of the json
 def lightupMatrix(data):
-    for key, value in data.items():
-        pixels[value] = hex_to_rgb(key)
+    for x in data['pixels'] :
+        pixel=int(x['name'])
+        pixels[pixel] = hex_to_rgb(x['value'])
 
  
 
- 
-lightupMatrix(data)
+data = '{"pixels":[{"name":"1","value":"#FFFFFF"},{"name":"4","value":"#B12345"},{"name":"22","value":"#FFFFFF"},{"name":"23","value":"#FFFFFF"},{"name":"34","value":"#FFFFFF"}]}'
+Loaddata=json.loads(data)
+lightupMatrix(Loaddata)
