3b233a2ed81eda22b1d5d88c5d5eba2b203bee9a
ChillImWonder <rantz.167@gmail.com>
2022-10-28 13:20:31 -0500

    updated createPresets function

--- API/filesystem.py
+++ API/filesystem.py
@@ -3,6 +3,15 @@ import json
 import numpy as np
 from PIL import Image as im
 
+presetsDir = os.path.dirname(__file__) + '/presets/'
+presetImgsDir = presetsDir + '/presetImgs/'
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    rgbList = []
+    for i in (0, 2, 4):
+        rgbList.append(int(value[i:i + 2], 16))
+    return rgbList
 
 presetsDir = os.path.dirname(__file__) + '/presets/'
 presetImgsDir = presetsDir + '/presetImgs/'
@@ -46,17 +55,31 @@ def retrievePresetLists():
 def createPreset(values):
     newPreset = {
         "name": "{}".format(values['presetName']),
-        "pixels": []
+        "pixels": values['pixels']
     }
     p = open('presets/{}.json'.format(newPreset["name"]), "w")
     p.write(json.dumps(newPreset))
     p.close()
 
-    array = np.arange(0, 256, 1, np.uint8)
-    array = np.reshape(array, (16, 16))
-    data = im.fromarray(array)
-    data.save(presetImgsDir + 'gfg_dummy_pic.png')
 
+    colors = np.empty([16, 16], dtype=list)
+
+    # int(len(values['pixels']) ** (1/2))
+    for i in range(16):
+        for j in range(16):
+            colors[i][j] = hex_to_rgb(values['pixels'][i + j]['value'])
+    print(colors)
+
+    image = im.fromarray(colors, "RGB")
+    image.save(presetImgsDir + '{}.png'.format(values['presetName']))
+
+    #array = im.fromarray(data)
+    #print(data)
+    #array = np.array(values['value'])
+    #narray = np.array(values['pixels'])
+    #array = np.arange(0, 256, 1, np.uint8)
+    #array = np.reshape(array, (16, 16))
+    #data = im.fromarray(array)
     # parse the json to get the name and pixel values
     # make the file with the name
     # insert in a dummy image file path
