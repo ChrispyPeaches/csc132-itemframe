ea8007a01794c8692155004dbf696433f96ee657
ChrispyPeaches <gameboy9478@gmail.com>
2022-10-28 13:43:39 -0500

    The image creation is closer to working

--- API/filesystem.py
+++ API/filesystem.py
@@ -6,12 +6,14 @@ from PIL import Image as im
 presetsDir = os.path.dirname(__file__) + '/presets/'
 presetImgsDir = presetsDir + '/presetImgs/'
 
+
 def hex_to_rgb(value):
     value = value.lstrip('#')
     rgbList = []
     for i in (0, 2, 4):
         rgbList.append(int(value[i:i + 2], 16))
-    return rgbList
+    return tuple(rgbList)
+
 
 presetsDir = os.path.dirname(__file__) + '/presets/'
 presetImgsDir = presetsDir + '/presetImgs/'
@@ -57,24 +59,26 @@ def createPreset(values):
         "name": "{}".format(values['presetName']),
         "pixels": values['pixels']
     }
-    p = open('presets/{}.json'.format(newPreset["name"]), "w")
+    p = open(presetsDir + '{}.json'.format(newPreset["name"]), "w")
     p.write(json.dumps(newPreset))
     p.close()
-
 
     colors = np.empty([16, 16], dtype=list)
 
     # int(len(values['pixels']) ** (1/2))
     for i in range(16):
         for j in range(16):
-            colors[i][j] = hex_to_rgb(values['pixels'][i + j]['value'])
+            colors[i, j] = hex_to_rgb(values['pixels'][i + j]['value'])
     print(colors)
 
     image = im.fromarray(colors, "RGB")
+    for i in range(16):
+        for j in range(16):
+            print(image.getpixel((i, j)))
     image.save(presetImgsDir + '{}.png'.format(values['presetName']))
 
     #array = im.fromarray(data)
-    #print(data)
+    # print(data)
     #array = np.array(values['value'])
     #narray = np.array(values['pixels'])
     #array = np.arange(0, 256, 1, np.uint8)
