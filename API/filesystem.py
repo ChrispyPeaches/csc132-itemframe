d1158ec8291e0147621c5c42c2566922edaad4e5
ChillImWonder <rantz.167@gmail.com>
2022-11-01 14:10:43 -0500

    upload image functionality

--- API/filesystem.py
+++ API/filesystem.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+from itertools import chain
 from PIL import Image as im
 
 presetsDir = os.path.join(os.path.dirname(__file__), "presets/")
@@ -52,19 +53,55 @@ def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % rgb
 
 
-def createDict(image):
+def createDict(name, image):
     raw_image = im.open(image)
     raw_image = raw_image.convert('RGB')
     pix_val = list(raw_image.getdata())
-    pixels = []
+    preset = {
+        "presetName": "{}".format(name.replace(' ', '_')),
+        "pixels": []
+    }
+
+    preset2 = {
+        "presetName": "{}".format(name.replace(' ', '_')),
+        "pixels": []
+    }
+
+    row = []
+    start = 0
+    end = 256
+    step = 16
+
+    for i in range(start, end, step):
+        x = i
+        row.append((pix_val[x:x + step]))
+
+    row[0].reverse()
+    row[2].reverse()
+    row[4].reverse()
+    row[6].reverse()
+    row[8].reverse()
+    row[10].reverse()
+    row[12].reverse()
+    row[14].reverse()
+
+    finVal = list(chain.from_iterable(row))
+    for index, val in enumerate(finVal):
+        t = {
+            f"name": f"pix[{index}]",
+            f"value": f"{rgb_to_hex((val))}"
+        }
+        preset['pixels'].append(t)
+    jsonPix = json.dumps(preset)
+
     for index, val in enumerate(pix_val):
         t = {
             f"name": f"pix[{index}]",
-            f"value" : f"{rgb_to_hex((val))}"
+            f"value": f"{rgb_to_hex(val)}"
         }
-        pixels.append(t)
-        jsonPix = json.dumps(pixels)
-    return(jsonPix)
+        preset2['pixels'].append(t)
+    jsonPix2 = json.dumps(preset2)
+    return(jsonPix, jsonPix2)
 
 
 def createPreset(values):
@@ -76,28 +113,41 @@ def createPreset(values):
     p.write(json.dumps(newPreset))
     p.close()
 
-    colors = np.empty((16, 16,3), dtype=np.uint8)
+
+    # parse the json to get the name and pixel values
+    # make the file with the name
+    # insert in a dummy image file path
+    # insert the pixel value list into the file
+
+def presetImg(values):
+    colors = np.empty((16, 16, 3), dtype=np.uint8)
 
     # int(len(values['pixels']) ** (1/2))
     for i in range(16):
         for j in range(16):
-            colors[i,j] = (hex_to_rgb(values['pixels'][16 * i + j]['value']))
-    #colors = np.array(colors, dt)
-    #colors.reshape((16,16,3))
-    print(colors)
+            colors[i, j] = (hex_to_rgb(values['pixels'][16 * i + j]['value']))
+
     image = im.fromarray(colors, "RGB")
-    for i in range(16):
-        for j in range(16):
-            print(image.getpixel((i, j)))
+    # for i in range(16):
+    # for j in range(16):
+    # print(image.getpixel((i, j)))
     image.save(presetImgsDir + '{}.png'.format(values['presetName'].replace(' ', '_')), 'PNG')
 
-    #array = im.fromarray(data)
-    # print(data)
-    #array = np.array(values['value'])
-    #narray = np.array(values['pixels'])
-    #array = np.arange(0, 256, 1, np.uint8)
-    #array = np.reshape(array, (16, 16))
-    #data = im.fromarray(array)
+
+def uploadedPresetImg(values):
+    colors = np.empty((16, 16, 3), dtype=np.uint8)
+
+    # int(len(values['pixels']) ** (1/2))
+    for i in range(16):
+        for j in range(16):
+            colors[i, j] = (hex_to_rgb(values['pixels'][16 * i + j]['value']))
+
+    image = im.fromarray(colors, "RGB")
+    # for i in range(16):
+    # for j in range(16):
+    # print(image.getpixel((i, j)))
+    image.save(presetImgsDir + '{}.png'.format(values['presetName'].replace(' ', '_')), 'PNG')
+
     # parse the json to get the name and pixel values
     # make the file with the name
     # insert in a dummy image file path
